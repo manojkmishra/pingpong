47548a40d246901e0e4d8e1947efc73af90306dd
manojkmishra <manojmishra571980@gmail.com>
2019-08-12 12:17:53 +1000

    Login page created, router installed, login accessed in app.js via /login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 import "./css/tailwind.css" ;
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from './Login';
 
 function App() {
   return (
     <div >
         <h1 className="bg-purple-500">  Tailwind css working </h1>
+        <Router>
+           <Route path="/login" component={Login} />
+        </Router>
     </div>
   );
 }
