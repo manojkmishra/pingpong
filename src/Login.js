a500364db864eab1e958da2bf7fdaf0294daba06
manojkmishra <manojmishra571980@gmail.com>
2019-08-12 14:35:03 +1000

    dynamic binding handled, values available in this.state

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,44 @@
 import React , {Component} from "react";
 
 export default class Login extends Component 
-{
+{   
+    constructor(props){
+        super(props);
+        this.state={email:'',password:'',errors:{}}
+    }
+    handleForm = (e) => {   //arrow function
+         e.preventDefault();
+         
+       }
+    handleInput =(e) => {
+        e.preventDefault();
+        const name= e.target.name;
+        const value=e.target.value;
+        this.setState({[name]:value});  //dynamic binding
+        console.log('e',e); 
+        console.log('state',this.state); 
+        console.log('props',this.props); 
+       // console.log(e.target.value);  //target is email text field
+    }
+
     render()
-    { return(
+    {         
+        return(
              <div>
                <h1> Login Page</h1>
                <div className="flex">
                  <div className="w-1/3"></div>  {/* this one third page column is just for left gap  */}
                  <div className="w-1/3 mt-10 p-4 bg-yellow-300">   {/* mt- margin top, p-padding  */}
-                  <form className="border border-gray-500">
+                  <form className="border border-gray-500" onSubmit={this.handleForm}>
                     <div className="p-4">  {/* tcontainer for below input, padding=4  */}
                       <h1 className="text-lg border-b border-gray-400">Login Form</h1>  {/*border-b =  border of bottom  */}
                       <div className="mt-4"> 
                           <label>Email</label>
-                          <input type="email" name="email" placeholder="email" className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full"></input>
+                          <input type="email" name="email" placeholder="email" onChange={this.handleInput } className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full"></input>
                       </div>
                       <div className="mt-4"> 
                           <label>Password</label>
-                          <input type="password" name="password" placeholder="password" className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full"></input>
+                          <input type="password" name="password" placeholder="password" onChange={this.handleInput } className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full"></input>
                       </div>
                       <div className="mt-4"> 
                           <input type="submit" className="mt-1 p-2 border border-gray-400 rounded cursor-pointer bg-purple-600 text-white"></input>
@@ -28,8 +48,7 @@ export default class Login extends Component
                   </div>
                </div>
              </div> 
-           
-           
+
            );
     }
 }
