5603f9626dc8f6583bf7a4ee2a20a65b2d1ea495
manojkmishra <manojmishra571980@gmail.com>
2019-08-20 12:23:24 +1000

    guestroute connected with redux and working on loggedIn value

--- src/components/GuestRoute.js
+++ src/components/GuestRoute.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Route, Redirect, } from "react-router-dom";
 import cookie from 'js-cookie';
+import { connect } from 'react-redux';
 
 const GuestRoute = ({ component: Component, ...rest }) => {
-    const token = cookie.get('token')
+    const token = cookie.get('token')  //not required anymore
     console.log('inside guestroute.js token=',token);
   
     return (  
       <Route  {...rest}
         render={props => /* if token is not present remain on same coponent else redirect to profile component */
-          !token ? ( <Component {...props } /> ) : (  <Redirect to={{  pathname: "/profile", state: { from: props.location }    }}   />   )
+          !rest.loggedIn ? ( <Component {...props } /> ) : (  <Redirect to={{  pathname: "/profile", state: { from: props.location }    }}   />   )
         }
       />
     );
   };
 
-  export default GuestRoute;
+  //export default GuestRoute;
+  const mapStateToProps = state=>{  console.log('mapDispatchToProps from login');   //2- dispatch action name=setLogin, type(mutation=SET_LOGIN) and values=user
+  return {loggedIn: state.auth.loggedIn}
+}
+export default connect (mapStateToProps)(GuestRoute);    //1-connect login page and two arguments
